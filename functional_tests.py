07e34b5297c27e84632a80c8a9b482064117daa0
blind <denislee98@gmail.com>
2020-01-10 23:56:20 +0300

    добавлен тест на провеку ввода дела в ФТ

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 import unittest
+import time
+from selenium.webdriver.common.keys import Keys
 
 class NewUserSite(unittest.TestCase):
 	'''тест нового юзера'''
@@ -18,16 +20,30 @@ class NewUserSite(unittest.TestCase):
 		#Пользователь открывает гланвую страницу
 		self.browser.get('http://localhost:8000')
 
-		#Пользователь видит в заголовке название 'Список дел'
+		#Пользователь видит в заголовке и шапке сайта название 'Список дел'
 		self.assertIn('Список дел', self.browser.title)
-		self.fail('Закончить тест')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('Список дел', header_text)
+		
 
 		#На странице сразу же располагается форма для ввода дела
+		inputbox = self.browser.find_element_by_id('new_item')
+		self.assertEqual('Новое дело', inputbox.get_attribute('placeholder'))
+
 		#Вводит какаое-ниудь дело (купить хлеб)
+		inputbox.send_keys('Купить хлеб')
 
-
-		#Нажимает на кнопку добавить
+		#Нажимает Enter
 		#Страница обновляется и появлется надпись "1. купить хлеб"
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		table = self.browser.find_element_by_id('list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.asserTrue(
+			any(row.text=='1: Купить хлеб' for row in rows)
+			)
+
 		#Форма для ввода также присутвует
 
 		#Пользователь вводит еще одно дело (полить цветы)
@@ -41,6 +57,7 @@ class NewUserSite(unittest.TestCase):
 		#Пользователь посещает url адрес и спсок дел остается
 
 		#Пользователь покидает сайт
+		self.fail('Закончить тест')
 
 if __name__=='__main__':
 	unittest.main(warnings='ignore')
